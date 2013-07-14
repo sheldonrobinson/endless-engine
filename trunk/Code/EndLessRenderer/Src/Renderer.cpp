f15236df86881026718431f59783f7a8552404ba
kaiyan.sjtu@gmail.com <kaiyan.sjtu@gmail.com@e887f20e-e8f0-411e-cf26-cf15dcdffb19>
2013-07-14 07:55:34 +0000

    windows窗口创建

---
+++ trunk/Code/EndLessRenderer/Src/Renderer.cpp
@@ -0,0 +1,23 @@
+//Renderer.cpp: Endless Renderer
+
+#include "Renderer.h"
+
+EEndlessRenderer::EEndlessRenderer()
+{
+
+}
+
+EEndlessRenderer::~EEndlessRenderer()
+{
+
+}
+
+bool EEndlessRenderer::Init(ENDLESS_RENDERER_DESC& desc)
+{
+	return true;
+}
+
+void EEndlessRenderer::Render()
+{
+
+}
